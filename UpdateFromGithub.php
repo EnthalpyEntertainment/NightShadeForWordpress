05382ee30aef15f5a5a33dedd4c18c84a4c10501
EnthalpyEntertainment <endofelnthalpyemail@gmail.com>
2026-05-01 12:28:00 +1000

    Update UpdateFromGithub.php

--- UpdateFromGithub.php
+++ UpdateFromGithub.php
@@ -1,1 +1,13 @@
+require_once __DIR__ . '/plugin-update-checker-master/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$updateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/EnthalpyEntertainment/NightShadeForWordpress/',
+    __FILE__,
+    'nightshade-for-wordpress'
+);
+
+// Set branch
+$updateChecker->setBranch('main');
 
